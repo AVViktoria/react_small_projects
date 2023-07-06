b3777426a530364a8d5cc0c3931801d9a260041b
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-07-06 16:10:31 -0700

    feat: create base markup and styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const onClickPlus = ()=>
-{
-  setCount(count+1);
-}
-const onClickMinus = ()=>
-{
-  setCount(count-1);
-}
   return (
-
-
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick = {onClickMinus} className="minus">- Минус</button>
-        <button onClick = {onClickPlus} className="plus">Плюс +</button>
-      </div>
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
