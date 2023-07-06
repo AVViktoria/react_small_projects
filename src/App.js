baf1239b9838bb76549cc68422081caa37cb9891
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-07-06 16:02:22 -0700

    feat: finish users task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+import { Success } from "./components/Success/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
+  // console.log(users);
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  // запрос данных из input
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+    console.log(searchValue);
+  };
+
+  // добавляем пользователей с минусом (выбранных) по id в массив
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  //передаем
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          items={users}
+          isLoading={isLoading}
+          searchValue={searchValue}
+          onChangeSearchValue={onChangeSearchValue}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Skeleton } from './Skeleton';
-import { User } from './User';
+import React from "react";
+import { Skeleton } from "./Skeleton";
+import { User } from "./User";
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({
+  items,
+  isLoading,
+  searchValue,
+  onChangeSearchValue,
+  onClickInvite,
+  invites,
+  onClickSendInvites,
+}) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+
+        <input
+          value={searchValue}
+          onChange={onChangeSearchValue}
+          type="text"
+          placeholder="Найти пользователя..."
+        />
       </div>
+      {/* isLoading- фейковая загрузка, скелетон */}
       {isLoading ? (
         <div className="skeleton-list">
           <Skeleton />
@@ -19,10 +34,40 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items
+            //фильтруем наших пользователей по введенному в search
+            .filter((obj) => {
+              const fullName = (obj.first_name + obj.last_name).toLowerCase();
+              const email = obj.email.toLowerCase();
+              const searchVal = searchValue.toLowerCase();
+
+              return (
+                fullName.includes(searchVal) ||
+                email.toLowerCase().includes(searchVal)
+              );
+            })
+            // выводим на экран или всех или отфильтрованных
+            .map((obj) => (
+              <User
+                onClickInvite={onClickInvite}
+                isInvited={invites.includes(obj.id)}
+                key={obj.id}
+                {...obj}
+              />
+            ))}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {invites.length > 0 && (
+        <button  onClick={onClickSendInvites} className="send-invite-btn">
+          Отправить приглашение
+        </button>
+      )}
     </>
   );
 };
+
+// распыление {...obj} - то же самое что и ниже
+// first_name={obj.first_name}
+// last_name={obj.last_name}
+// email={obj.email}
+// avatar={obj.avatar}
