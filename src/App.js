68434fbabb3d18c2873f3f5c7b8813892e6f012a
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-07-07 12:07:47 -0700

    feat: need to fix, try to add useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, userRef } from "react";
 import { Block } from "./Block";
 import "./index.scss";
 // import data from "./data/data.json";
 function App() {
   const [fromCurrency, setFromCurrency] = useState("UAH");
   const [toCurrency, setToCurrency] = useState("USD");
-  const [fromPrice, setFromPrice] = useState(0);
+  const [fromPrice, setFromPrice] = useState(1);
   const [toPrice, setToPrice] = useState(0);
 
+  //вместо useState делаем hook ratesRef
   const [rates, setRates] = useState({});
   // console.log(rates);
+
+  // const ratesRef = userRef({});
+
   useEffect(() => {
     fetch("https://www.cbr-xml-daily.ru/daily_json.js")
       .then((res) => res.json())
       .then((json) => {
+        // setRates асинхронная функция
         setRates(json.Valute);
-        // console.log(json.Valute);
+        // ratesRef.current = json.Valute;
+        onChangeToPrice(1);
       })
       .catch((err) => {
         console.warn(err);
@@ -26,9 +32,8 @@ function App() {
   const onChangeFromPrice = (value) => {
     const price = value / rates[fromCurrency].Value;
     const result = price * rates[toCurrency].Value;
-    // console.log(rates[fromCurrency].Value);
     // сделано без учета номинала rates[fromCurrency].Nominal;
-    setToPrice(result);
+    setToPrice(result.toFixed(3));
     setFromPrice(value);
   };
 
@@ -36,7 +41,7 @@ function App() {
     const result =
       (rates[fromCurrency].Value / rates[toCurrency].Value) * value;
     // сделано без учета номинала rates[fromCurrency].Nominal;
-    setFromPrice(result);
+    setFromPrice(result.toFixed(3));
     setToPrice(value);
   };
   const onChangeFromCurrency = (cur) => {
@@ -51,24 +56,24 @@ function App() {
   //need to fix, because work with two presses on valute name
   // useEffect(() => {
   //   onChangeFromPrice(fromPrice);
-  // }, [fromCurrency, fromPrice]);
+  // }, [fromCurrency]);
 
   // useEffect(() => {
   //   onChangeToPrice(toPrice);
-  // }, [toCurrency, toPrice]);
+  // }, [toCurrency]);
 
   return (
     <div className="App">
       <Block
         value={fromPrice}
         currency={fromCurrency}
-        onChangeCurrency={onChangeFromCurrency} //setFromCurrency
+        onChangeCurrency={onChangeFromCurrency} //setFromCurrency   onChangeFromCurrency
         onChangeValue={onChangeFromPrice}
       />
       <Block
         value={toPrice}
         currency={toCurrency}
-        onChangeCurrency={onChangeToCurrency} //setToCurrency
+        onChangeCurrency={onChangeToCurrency} //setToCurrency  onChangeToCurrency
         onChangeValue={onChangeToPrice}
       />
     </div>
@@ -76,3 +81,40 @@ function App() {
 }
 
 export default App;
+
+// const onChangeFromPrice = React.useCallback(
+//   (value) => {
+//     const price = value / ratesRef.current[fromCurrency].Value;
+//     const result = price * ratesRef.current[toCurrency].Value;
+//     // сделано без учета номинала rates[fromCurrency].Nominal;
+//     setToPrice(result);
+//     setFromPrice(value);
+//   },
+//   [fromCurrency, toCurrency, ratesRef]
+// );
+// const onChangeToPrice = React.useCallback(
+//   (value) => {
+//     const result =
+//       (ratesRef.current[fromCurrency].Value /
+//         ratesRef.current[toCurrency].Value) *
+//       value;
+//     // сделано без учета номинала rates[fromCurrency].Nominal;
+//     setFromPrice(result);
+//     setToPrice(value);
+//   },
+//   [fromCurrency, toCurrency, ratesRef]
+// );
+// useEffect(() => {
+//   fetch("https://www.cbr-xml-daily.ru/daily_json.js")
+//     .then((res) => res.json())
+//     .then((json) => {
+//       // setRates асинхронная функция
+//       // setRates(json.Valute);
+//       ratesRef.current = json.Valute;
+//       onChangeToPrice(1);
+//     })
+//     .catch((err) => {
+//       console.warn(err);
+//       alert("Can't find any information");
+//     });
+// }, [ratesRef, onChangeToPrice]);
