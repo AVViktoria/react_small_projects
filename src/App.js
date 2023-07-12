180b7660dd5f9e92a587daae13c8199458eea16b
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-07-11 20:28:27 -0700

    feat: create  filter

--- src/App.js
+++ src/App.js
@@ -2,33 +2,60 @@ import { useEffect, useState } from "react";
 import { Collection } from "./Collection";
 import "./index.scss";
 
+const cats = [
+  { name: "Все" },
+  { name: "Море" },
+  { name: "Горы" },
+  { name: "Архитектура" },
+  { name: "Города" },
+];
+
 function App() {
+  // сортируем по категориям
+  const [categoryId, setCategoryId] = useState(0);
+
   // контролируемый импут
   const [searchValue, setSearchValue] = useState("");
 
+  // оформляем фетч
   const [collections, setCollections] = useState([]);
+
   useEffect(() => {
-    fetch("https://64a78152096b3f0fcc8161fd.mockapi.io/photos")
+    fetch(
+      `https://64a78152096b3f0fcc8161fd.mockapi.io/photos?${
+        categoryId ? `category = ${categoryId}` : ""
+      }`
+    )
       .then((res) => res.json())
       .then((json) => {
         setCollections(json);
       })
       .catch((err) => {
-        console.log(err);
+        console.warn(err);
         alert("Oшибка при получении данных");
       });
-  }, []);
+  }, [categoryId]);
 
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
+          {/* <li className="active">Все</li> */}
+          {/* отображаем категории  */}
+          {cats.map((obj, i) => (
+            <li
+              onClick={() => setCategoryId(i)}
+              className={categoryId === i ? "active" : ""}
+              key={obj.name}
+            >
+              {obj.name}
+            </li>
+          ))}
+          {/* <li>Горы</li>
           <li>Море</li>
           <li>Архитектура</li>
-          <li>Города</li>
+          <li>Города</li> */}
         </ul>
         <input
           value={searchValue}
